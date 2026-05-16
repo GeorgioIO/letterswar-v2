bee68952f50801e0f22e7446f95bb2aed8f4b2f5
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-05-16 16:36:40 +0300

    Change db and add ssl

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,9 +5,17 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME || "test",
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default pool;
