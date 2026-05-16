3b86b9091c3f344c2b4c760ad3a46d97c3c75498
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-05-16 22:39:44 +0300

    remove authenticate middleware from logout route

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -10,7 +10,7 @@ import { authenticate } from "../../middleware/auth.js";
 const router = Router();
 
 router.post("/login", LoginLimitter, loginController);
-router.post("/logout", authenticate, logoutController);
+router.post("/logout", logoutController);
 router.get("/me", authenticate, meController);
 
 export default router;
