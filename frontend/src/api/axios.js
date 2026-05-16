c90a81f3897afed2b8f36070160bf70bcdebe219
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-05-16 17:26:03 +0300

    Change axios url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  // This checks if VITE_API_URL exists (Vercel), otherwise falls back to localhost
+  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api/v1`,
   withCredentials: true,
 });
 
