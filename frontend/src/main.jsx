a97663001a954975fe75438f7913a3251711606a
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-05-28 19:54:34 +0300

    Added query Client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,22 @@
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App.jsx";
 import AuthContextProvider from "./store/AuthContext.jsx";
 import ToastContextProvider from "./store/ToastContext.jsx";
 import store from "./store/store.js";
-import { Provider } from "react-redux";
+import { queryClient } from "./util/tanstack.js";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <ToastContextProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </ToastContextProvider>
-  </Provider>,
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <ToastContextProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </ToastContextProvider>
+    </Provider>
+  </QueryClientProvider>,
 );
