75d6ffb964cf6057cdb29d686a373cb35fc20c45
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-05-30 13:52:50 +0300

    Test

--- frontend/src/api/stats.api.js
+++ frontend/src/api/stats.api.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
 export async function getAllStatsRequest(signal) {
-  const response = await api.get("/sts", { signal });
+  const response = await api.get("/stats", { signal });
   return response.data;
 }
